492e87ddc5bd97c2fb8f970b7a17a2bb9b2ad465
Raytlty <alexmao930918@gmail.com>
2022-01-11 23:14:10 +0800

    udpate ice_transport

--- bns-core/src/ice_transport.rs
+++ bns-core/src/ice_transport.rs
@@ -8,8 +8,10 @@ use tokio::sync::mpsc::channel;
 use tokio::sync::mpsc::Receiver;
 use tokio::sync::mpsc::Sender;
 use tokio::sync::Mutex;
+use tokio::time::Duration;
 use webrtc::api::APIBuilder;
 use webrtc::api::API;
+use webrtc::data_channel::data_channel_message::DataChannelMessage;
 use webrtc::data_channel::OnOpenHdlrFn;
 use webrtc::data_channel::RTCDataChannel;
 use webrtc::ice_transport::ice_candidate::RTCIceCandidate;
@@ -27,141 +29,123 @@ use webrtc::peer_connection::RTCPeerConnection;
 
 #[derive(Clone)]
 pub struct IceTransport {
-    pub conn: Arc<Mutex<Option<Arc<RTCPeerConnection>>>>,
-    pub remote_session_description: Option<RTCSessionDescription>,
-    pub local_session_description: Option<RTCSessionDescription>,
-    pub connection_state: Arc<Mutex<RTCPeerConnectionState>>,
-    pub ice_candidates: Arc<Mutex<Vec<RTCIceCandidate>>>,
+    pub connection: Arc<Mutex<Option<Arc<RTCPeerConnection>>>>,
+    pub pending_candidates: Arc<Mutex<Vec<RTCIceCandidate>>>,
 }
 
 impl IceTransport {
-    pub async fn new(urls: Vec<String>) -> Result<Self> {
-        let mut urls = urls;
-        if urls.len() <= 0 {
-            urls = vec!["stun:stun.l.google.com:19302".to_owned()];
-        }
-
-        let api = APIBuilder::new().build();
+    pub async fn new() -> Result<Self> {
         let config = RTCConfiguration {
             ice_servers: vec![RTCIceServer {
-                urls: urls,
+                urls: vec!["stun:stun.l.google.com:19302".to_owned()],
                 ..Default::default()
             }],
             ..Default::default()
         };
+        let api = APIBuilder::new().build();
         let peer_connection = Arc::new(api.new_peer_connection(config).await?);
-        let connection_state = peer_connection.connection_state();
-
         Ok(IceTransport {
-            conn: Arc::new(Mutex::new(Some(Arc::clone(&peer_connection)))),
-            remote_session_description: None,
-            local_session_description: None,
-            connection_state: Arc::new(Mutex::new(connection_state)),
-            ice_candidates: Arc::new(Mutex::new(vec![])),
+            connection: Arc::new(Mutex::new(Some(Arc::clone(&peer_connection)))),
+            pending_candidates: Arc::new(Mutex::new(vec![])),
         })
     }
 
-    pub async fn add_ice_candidate(&self, candidate: String) {
-        if let Err(e) = self
-            .conn
-            .lock()
-            .await
-            .clone()
-            .unwrap()
-            .add_ice_candidate(RTCIceCandidateInit {
-                candidate,
-                ..Default::default()
-            })
-            .await
-        {
-            panic!("{}", e);
-        }
-    }
-
-    pub async fn set_remote_description(&self, sdp: RTCSessionDescription) {
-        if let Err(e) = self
-            .conn
-            .lock()
-            .await
-            .clone()
-            .unwrap()
-            .set_remote_description(sdp)
-            .await
-        {
-            panic!("LocalDescription Failed, {:?}", e);
-        }
-    }
-
-    pub async fn set_local_description(&self, sdp: RTCSessionDescription) {
-        if let Err(e) = self
-            .conn
-            .lock()
-            .await
-            .clone()
-            .unwrap()
-            .set_local_description(sdp)
-            .await
-        {
-            panic!("RemoteDescription Failed");
-        }
-    }
-
-    pub async fn on_ice_candidate(
-        &mut self,
-        addr: String,
-        f: Arc<
-            Box<
-                dyn Fn(
-                        String,
-                        RTCIceCandidate,
-                    )
-                        -> Pin<Box<dyn Future<Output = Result<()>> + Send + 'static>>
-                    + Send
-                    + Sync,
-            >,
-        >,
-    ) -> Result<()> {
-        let addr = Arc::new(addr);
-        let pc = Arc::downgrade(&self.conn.lock().await.clone().unwrap());
-        let pending_candidates2 = Arc::clone(&self.ice_candidates);
-        let f2 = Arc::clone(&f);
-        self.conn
+    pub async fn start_answer(&self, candidate_tx: Sender<RTCIceCandidate>) {
+        let pc = Arc::downgrade(&self.connection.lock().await.clone().unwrap());
+        let pending_candidates2 = Arc::clone(&self.pending_candidates);
+        let candidate_tx2 = candidate_tx.clone();
+        self.connection
             .lock()
             .await
             .clone()
             .unwrap()
             .on_ice_candidate(Box::new(move |c: Option<RTCIceCandidate>| {
-                println!("on ice candidate {:?}", c);
+                println!("start answer candidate option: {:?}", c);
                 let pc2 = pc.clone();
                 let pending_candidates3 = Arc::clone(&pending_candidates2);
-                let addr2 = addr.clone();
-                let f3 = Arc::clone(&f2);
+                let candidate_tx3 = candidate_tx2.clone();
                 Box::pin(async move {
-                    if let Some(c) = c {
+                    if let Some(candidate) = c {
                         if let Some(pc) = pc2.upgrade() {
                             let desc = pc.remote_description().await;
                             if desc.is_none() {
-                                let mut cs = pending_candidates3.lock().await;
-                                cs.push(c.clone());
-                            } else if let Err(e) = f3(addr2.to_string(), c).await {
-                                panic!("On Ice Candidate F Failed, {:?}", e);
+                                let mut candidates = pending_candidates3.lock().await;
+                                println!("start answer candidate: {:?}", candidate);
+                                candidates.push(candidate.clone());
+                                candidate_tx3.send(candidate).await;
                             }
                         }
                     }
                 })
             }))
             .await;
-        Ok(())
-    }
 
-    pub async fn on_data_channel(&self, f: OnDataChannelHdlrFn) -> Result<()> {
-        self.conn
+        let (done_tx, mut done_rx) = tokio::sync::mpsc::channel::<()>(1);
+        self.connection
             .lock()
             .await
             .clone()
             .unwrap()
-            .on_data_channel(f)
+            .on_peer_connection_state_change(Box::new(move |s: RTCPeerConnectionState| {
+                // Failed to exit dial server
+                if s == RTCPeerConnectionState::Failed {
+                    let _ = done_tx.try_send(());
+                }
+
+                Box::pin(async {})
+            }))
             .await;
-        Ok(())
+        // server on data_channel open
+        // or return data-channel object
+        // demo should just ping pong
+        self.connection
+            .lock()
+            .await.clone().unwrap()
+            .on_data_channel(Box::new(move |d: Arc<RTCDataChannel>| {
+                let d_label = d.label().to_owned();
+                let d_id = d.id();
+                println!("New DataChannel {} {}", d_label, d_id);
+
+                Box::pin(async move{
+                    // Register channel opening handling
+                    let d2 =  Arc::clone(&d);
+                    let d_label2 = d_label.clone();
+                    let d_id2 = d_id;
+                    d.on_open(Box::new(move || {
+                        println!("Data channel '{}'-'{}' open. Random messages will now be sent to any connected DataChannels every 5 seconds", d_label2, d_id2);
+                        Box::pin(async move {
+                            let mut result = Result::<usize>::Ok(0);
+                            while result.is_ok() {
+                                let timeout = tokio::time::sleep(Duration::from_secs(5));
+                                tokio::pin!(timeout);
+
+                                tokio::select! {
+                                    _ = timeout.as_mut() =>{
+                                        let message = math_rand_alpha(15);
+                                        println!("Sending '{}'", message);
+                                        result = d2.send_text(message).await.map_err(Into::into);
+                                    }
+                                };
+                            }
+                        })
+                    })).await;
+
+                    // Register text message handling
+                    d.on_message(Box::new(move |msg: DataChannelMessage| {
+                        let msg_str = String::from_utf8(msg.data.to_vec()).unwrap();
+                        println!("Message from DataChannel '{}': '{}'", d_label, msg_str);
+                        Box::pin(async{})
+                    })).await;
+                })
+        })).await;
+
+        tokio::select! {
+            _ = done_rx.recv() => {
+                println!("received done signal!");
+            }
+            _ = tokio::signal::ctrl_c() => {
+                println!("");
+            }
+        };
     }
 }
--- bns-core/src/main.rs
+++ bns-core/src/main.rs
@@ -1,5 +1,6 @@
 use anyhow::Result;
 use clap::{App, AppSettings, Arg};
+use hyper::client::connect::HttpInfo;
 use hyper::service::{make_service_fn, service_fn};
 use hyper::{Body, Client, Method, Request, Response, Server, StatusCode};
 use std::io::Write;
@@ -8,10 +9,15 @@ use std::str::FromStr;
 use std::sync::Arc;
 use tokio::sync::Mutex;
 use tokio::time::Duration;
+use webrtc::api::interceptor_registry::register_default_interceptors;
+use webrtc::api::media_engine::MediaEngine;
+use webrtc::api::APIBuilder;
 use webrtc::data_channel::data_channel_message::DataChannelMessage;
 use webrtc::data_channel::RTCDataChannel;
-use webrtc::ice_transport::ice_candidate::RTCIceCandidateInit;
-use webrtc::ice_transport::ice_candidate::RTCIceCandidate;
+use webrtc::ice_transport::ice_candidate::{RTCIceCandidate, RTCIceCandidateInit};
+use webrtc::ice_transport::ice_server::RTCIceServer;
+use webrtc::interceptor::registry::Registry;
+use webrtc::peer_connection::configuration::RTCConfiguration;
 use webrtc::peer_connection::math_rand_alpha;
 use webrtc::peer_connection::peer_connection_state::RTCPeerConnectionState;
 use webrtc::peer_connection::sdp::session_description::RTCSessionDescription;
@@ -19,129 +25,7 @@ use webrtc::peer_connection::RTCPeerConnection;
 
 use bns_core::ice_transport::IceTransport;
 
-async fn signal_candidate2(addr: String, c: RTCIceCandidate) -> Result<()> {
-    let payload = c.to_json().await?.candidate;
-    let req = match Request::builder()
-        .method(Method::POST)
-        .uri(format!("http://{}/candidate", addr))
-        .header("content-type", "application/json; charset=utf-8")
-        .body(Body::from(payload))
-    {
-        Ok(req) => req,
-        Err(err) => {
-            println!("{}", err);
-            return Err(err.into());
-        }
-    };
-
-    let _resp = match Client::new().request(req).await {
-        Ok(resp) => resp,
-        Err(err) => {
-            println!("{}", err);
-            return Err(err.into());
-        }
-    };
-
-    Ok(())
-}
-
-async fn remote_handler2(
-    req: Request<Body>,
-    addr: String,
-    ice_transport: IceTransport,
-) -> Result<Response<Body>, hyper::Error> {
-    match (req.method(), req.uri().path()) {
-        (&Method::POST, "/candidate") => {
-            let candidate =
-                match std::str::from_utf8(&hyper::body::to_bytes(req.into_body()).await?) {
-                    Ok(s) => s.to_owned(),
-                    Err(err) => panic!("{}", err),
-                };
-
-            ice_transport.add_ice_candidate(candidate).await;
-            let mut response = Response::new(Body::empty());
-            *response.status_mut() = StatusCode::OK;
-            Ok(response)
-        }
-
-        (&Method::POST, "/sdp") => {
-            let sdp_str = match std::str::from_utf8(&hyper::body::to_bytes(req.into_body()).await?)
-            {
-                Ok(s) => s.to_owned(),
-                Err(err) => panic!("{}", err),
-            };
-            let sdp = match serde_json::from_str::<RTCSessionDescription>(&sdp_str) {
-                Ok(s) => s,
-                Err(err) => panic!("{}", err),
-            };
-
-            ice_transport.set_remote_description(sdp).await;
-
-            let answer = match ice_transport
-                .conn
-                .lock()
-                .await
-                .clone()
-                .unwrap()
-                .create_answer(None)
-                .await
-            {
-                Ok(a) => a,
-                Err(err) => panic!("{}", err),
-            };
-
-            println!("{:?}", answer);
-
-            let payload = match serde_json::to_string(&answer) {
-                Ok(p) => p,
-                Err(err) => panic!("{}", err),
-            };
-
-            let req = match Request::builder()
-                .method(Method::POST)
-                .uri(format!("http://{}/sdp", addr.clone()))
-                .header("content-type", "application/json; charset=utf-8")
-                .body(Body::from(payload))
-            {
-                Ok(req) => req,
-                Err(err) => panic!("{}", err),
-            };
-
-            let _resp = match Client::new().request(req).await {
-                Ok(resp) => resp,
-                Err(err) => {
-                    println!("{}", err);
-                    return Err(err);
-                }
-            };
-
-            ice_transport.set_local_description(answer).await;
-            {
-                let cs = ice_transport.ice_candidates.lock().await;
-                for c in &*cs {
-                    if let Err(err) = signal_candidate(addr.clone(), c).await {
-                        panic!("{}", err);
-                    }
-                }
-            }
-
-            let mut response = Response::new(Body::empty());
-            *response.status_mut() = StatusCode::OK;
-            Ok(response)
-        }
-        _ => {
-            let mut not_found = Response::default();
-            *not_found.status_mut() = StatusCode::NOT_FOUND;
-            Ok(not_found)
-        }
-    }
-}
-
-async fn signal_candidate(addr: String, c: &RTCIceCandidate) -> Result<()> {
-    /*println!(
-        "signal_candidate Post candidate to {}",
-        format!("http://{}/candidate", addr)
-    );*/
+async fn signal_candidate(addr: &str, c: &RTCIceCandidate) -> Result<()> {
     let payload = c.to_json().await?.candidate;
     let req = match Request::builder()
         .method(Method::POST)
@@ -168,31 +52,21 @@ async fn signal_candidate(addr: String, c: &RTCIceCandidate) -> Result<()> {
     Ok(())
 }
 
-// HTTP Listener to get ICE Credentials/Candidate from remote Peer
-async fn remote_handler(req: Request<Body>, ) -> Result<Response<Body>, hyper::Error> {
-    let pc = {
-        let pcm = PEER_CONNECTION_MUTEX.lock().await;
-        pcm.clone().unwrap()
-    };
-    let addr = {
-        let addr = ADDRESS.lock().await;
-        addr.clone()
-    };
-
+async fn remote_handler(
+    req: Request<Body>,
+    remote_addr: String,
+    ice_transport: IceTransport,
+) -> Result<Response<Body>, hyper::Error> {
+    let pc = ice_transport.connection.lock().await.clone().unwrap();
     match (req.method(), req.uri().path()) {
-        // A HTTP handler that allows the other WebRTC-rs or Pion instance to send us ICE candidates
-        // This allows us to add ICE candidates faster, we don't have to wait for STUN or TURN
-        // candidates which may be slower
         (&Method::POST, "/candidate") => {
-            //println!("remote_handler receive from /candidate");
             let candidate =
                 match std::str::from_utf8(&hyper::body::to_bytes(req.into_body()).await?) {
                     Ok(s) => s.to_owned(),
                     Err(err) => panic!("{}", err),
                 };
-
             if let Err(err) = pc
-                .add_ice_candidate(RTCIceCandidateInit {&
+                .add_ice_candidate(RTCIceCandidateInit {
                     candidate,
                     ..Default::default()
                 })
@@ -206,9 +80,8 @@ async fn remote_handler(req: Request<Body>, ) -> Result<Response<Body>, hyper::E
             Ok(response)
         }
 
-        // A HTTP handler that processes a SessionDescription given to us from the other WebRTC-rs or Pion process
         (&Method::POST, "/sdp") => {
-            //println!("remote_handler receive from /sdp");
+            // maybe can handle json decode sdp and server addr
             let sdp_str = match std::str::from_utf8(&hyper::body::to_bytes(req.into_body()).await?)
             {
                 Ok(s) => s.to_owned(),
@@ -228,13 +101,7 @@ async fn remote_handler(req: Request<Body>, ) -> Result<Response<Body>, hyper::E
                 Ok(a) => a,
                 Err(err) => panic!("{}", err),
             };
-            println!("{:?}", answer);
-            /*println!(
-                "remote_handler Post answer to {}",
-                format!("http://{}/sdp", addr)
-            );*/
 
-            // Send our answer to the HTTP server listening in the other process
             let payload = match serde_json::to_string(&answer) {
                 Ok(p) => p,
                 Err(err) => panic!("{}", err),
@@ -242,7 +109,7 @@ async fn remote_handler(req: Request<Body>, ) -> Result<Response<Body>, hyper::E
 
             let req = match Request::builder()
                 .method(Method::POST)
-                .uri(format!("http://{}/sdp", addr))
+                .uri(format!("http://{}/sdp", remote_addr))
                 .header("content-type", "application/json; charset=utf-8")
                 .body(Body::from(payload))
             {
@@ -257,20 +124,10 @@ async fn remote_handler(req: Request<Body>, ) -> Result<Response<Body>, hyper::E
                     return Err(err);
                 }
             };
-            //println!("remote_handler Response: {}", resp.status());
 
             // Sets the LocalDescription, and starts our UDP listeners
             if let Err(err) = pc.set_local_description(answer).await {
                 panic!("{}", err);
-            }
-
-            {
-                let cs = PENDING_CANDIDATES.lock().await;
-                for c in &*cs {
-                    if let Err(err) = signal_candidate(addr.clone(), c).await {
-                        panic!("{}", err);
-                    }
-                }
             }
 
             let mut response = Response::new(Body::empty());
@@ -288,128 +145,28 @@ async fn remote_handler(req: Request<Body>, ) -> Result<Response<Body>, hyper::E
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let p0_addr = String::from("0.0.0.0:60000");
-    let p1_addr = String::from("0.0.0.0:50000");
-    let ice_transport = IceTransport::new(vec![]).await?;
-
-    let mut ice_transport_dc = ice_transport.clone();
-    let peer_connection = ice_transport_dc.conn.lock().await.clone().unwrap();
-    let pending_candidate = ice_transport_dc.clone().ice_candidates;
-
-    let pending_candidates2 = Arc::clone(&pending_candidate);
-    let pc = Arc::downgrade(&peer_connection);
-    peer_connection
-        .on_ice_candidate(Box::new(move |c: Option<RTCIceCandidate>| {
-            println!("on_ice_candidate {:?}", c);
-
-            let pc2 = pc.clone();
-            let pending_candidates3 = Arc::clone(&pending_candidates2);
-            let addr3 = p1_addr.clone();
-            Box::pin(async move {
-                if let Some(c) = c {
-                    if let Some(pc) = pc2.upgrade() {
-                        let desc = pc.remote_description().await;
-                        if desc.is_none() {
-                            let mut cs = pending_candidates3.lock().await;
-                            cs.push(c);
-                        } else if let Err(err) = signal_candidate(addr3, &c).await {
-                            assert!(false, "{}", err);
-                        }
-                    }
-                }
-            })
-        }))
-        .await;
+    let server_addr = "0.0.0.0:60000";
+    let remote_addr = "0.0.0.0:50000";
+    let ice_transport = IceTransport::new().await?;
+    let ice_transport_start = ice_transport.clone();
+    let (candidate_tx, mut candidate_rx) = tokio::sync::mpsc::channel::<RTCIceCandidate>(1);
     tokio::spawn(async move {
-        let addr = SocketAddr::from_str(&p0_addr).unwrap();
-        let service = make_service_fn(|_| {
-            let p0_addr2 = p0_addr.clone();
-            let ice_transport2 = ice_transport.clone();
-            async move { Ok::<_, hyper::Error>(service_fn(move |req| remote_handler(req, addr, ice_transport))) }
+        let addr = SocketAddr::from_str(&server_addr).unwrap();
+        let ice_transport2 = ice_transport.clone();
+        let service = make_service_fn(move |_| {
+            let ice_transport3 = ice_transport2.clone();
+            async move {
+                Ok::<_, hyper::Error>(service_fn(move |req| {
+                    remote_handler(req, remote_addr.to_string(), ice_transport3.clone())
+                }))
+            }
         });
         let server = Server::bind(&addr).serve(service);
-        println!("Run this server for ... forever!");
         // Run this server for... forever!
         if let Err(e) = server.await {
-            println!("server error: {}", e);
+            eprintln!("server error: {}", e);
         }
     });
-
-    let (done_tx, mut done_rx) = tokio::sync::mpsc::channel::<()>(1);
-
-    ice_transport_dc
-        .conn
-        .lock()
-        .await
-        .clone()
-        .unwrap()
-        .on_peer_connection_state_change(Box::new(move |s: RTCPeerConnectionState| {
-            println!("Peer Connection State has changed: {}", s);
-
-            if s == RTCPeerConnectionState::Failed {
-                println!("Peer Connection has gone to failed exiting");
-                let _ = done_tx.try_send(());
-            }
-
-            Box::pin(async {})
-        }))
-        .await;
-
-    ice_transport_dc.clone().on_data_channel(Box::new(move |d: Arc<RTCDataChannel>| {
-        let d_label = d.label().to_owned();
-        let d_id = d.id();
-        println!("New DataChannel {} {}", d_label, d_id);
-
-        Box::pin(async move{
-            // Register channel opening handling
-            let d2 =  Arc::clone(&d);
-            let d_label2 = d_label.clone();
-            let d_id2 = d_id;
-            d.on_open(Box::new(move || {
-                println!("Data channel '{}'-'{}' open. Random messages will now be sent to any connected DataChannels every 5 seconds", d_label2, d_id2);
-                Box::pin(async move {
-                    let mut result = Result::<usize>::Ok(0);
-                    while result.is_ok() {
-                        let timeout = tokio::time::sleep(Duration::from_secs(5));
-                        tokio::pin!(timeout);
-
-                        tokio::select! {
-                            _ = timeout.as_mut() =>{
-                                let message = math_rand_alpha(15);
-                                println!("Sending '{}'", message);
-                                result = d2.send_text(message).await.map_err(Into::into);
-                            }
-                        };
-                    }
-                })
-            })).await;
-
-            // Register text message handling
-            d.on_message(Box::new(move |msg: DataChannelMessage| {
-               let msg_str = String::from_utf8(msg.data.to_vec()).unwrap();
-               println!("Message from DataChannel '{}': '{}'", d_label, msg_str);
-               Box::pin(async{})
-           })).await;
-        })
-    })).await;
-
-    println!("Press ctrl-c to stop");
-    tokio::select! {
-        _ = done_rx.recv() => {
-            println!("received done signal!");
-        }
-        _ = tokio::signal::ctrl_c() => {
-            println!("");
-        }
-    };
-
-    ice_transport_dc
-        .conn
-        .lock()
-        .await
-        .clone()
-        .unwrap()
-        .close()
-        .await;
+    ice_transport_start.start_answer(candidate_tx).await;
     Ok(())
 }
