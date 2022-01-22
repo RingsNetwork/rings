1434ec26e8a67fe5adb25b19776b1bdc0530c2c0
magine <magine233@gmail.com>
2022-01-22 14:07:30 +0800

    Feature/cli with cmd line parser

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use bns_node::discoveries::http::remote_handler;
 use std::net::SocketAddr;
-use std::str::FromStr;
 use std::sync::Arc;
 use std::time::Duration;
 
@@ -8,6 +7,7 @@ use anyhow::Result;
 use bns_core::transports::default::DefaultTransport;
 use bns_core::types::ice_transport::IceTransport;
 use bns_core::types::ice_transport::IceTransportBuilder;
+use clap::Parser;
 use hyper::service::{make_service_fn, service_fn};
 use hyper::Server;
 use webrtc::data_channel::data_channel_message::DataChannelMessage;
@@ -16,10 +16,19 @@ use webrtc::ice_transport::ice_candidate::RTCIceCandidate;
 use webrtc::peer_connection::math_rand_alpha;
 use webrtc::peer_connection::peer_connection_state::RTCPeerConnectionState;
 
+#[derive(Parser, Debug)]
+#[clap(about, version, author)]
+struct Args {
+    #[clap(long, short = 'h', default_value = "0.0.0.0:60000")]
+    http_addr: String,
+
+    #[clap(long, short = 'r', default_value = "0.0.0.0:50000")]
+    remote_addr: String,
+}
+
 #[tokio::main]
 async fn main() -> Result<()> {
-    let http_addr = "0.0.0.0:60000";
-    let remote_addr = "0.0.0.0:50000";
+    let args = Args::parse();
 
     let mut ice_transport = DefaultTransport::new();
     ice_transport.start().await?;
@@ -97,17 +106,22 @@ async fn main() -> Result<()> {
 
     tokio::spawn(async move {
         let ice_transport = ice_transport.clone();
+        let remote_addr = args.remote_addr.clone();
+
         let service = make_service_fn(move |_| {
             let ice_transport = ice_transport.to_owned();
+            let remote_addr = remote_addr.to_owned();
+
             async move {
                 Ok::<_, hyper::Error>(service_fn(move |req| {
-                    remote_handler(req, remote_addr.to_string(), ice_transport.to_owned())
+                    remote_handler(req, remote_addr.to_owned(), ice_transport.to_owned())
                 }))
             }
         });
 
-        let http_addr = SocketAddr::from_str(http_addr).unwrap();
+        let http_addr: SocketAddr = args.http_addr.parse().unwrap();
         let server = Server::bind(&http_addr).serve(service);
+
         // Run this server for... forever!
         if let Err(e) = server.await {
             eprintln!("server error: {}", e);
