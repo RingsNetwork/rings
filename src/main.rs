1d062a58c4a1853246dd228abc98ab3354c8a066
magine <magine233@gmail.com>
2022-02-21 19:09:21 +0800

    Refactor/split service and drive by axum (#42)

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,12 @@
 #![feature(async_closure)]
 use anyhow::Result;
 use bns_core::channels::default::AcChannel;
+use bns_core::signing::SecretKey;
 use bns_core::swarm::Swarm;
 use bns_core::types::channel::Channel;
-//use bns_node::config::read_config;
-use bns_core::signing::SecretKey;
-use bns_node::discoveries::http::discoveries_services;
 use bns_node::logger::Logger;
+use bns_node::service::run_service;
 use clap::Parser;
-use hyper::service::{make_service_fn, service_fn};
-use hyper::Server;
-use std::net::SocketAddr;
 
 use std::sync::Arc;
 
@@ -19,9 +15,6 @@ use std::sync::Arc;
 pub struct Args {
     #[clap(long, short = 'd', default_value = "127.0.0.1:50000")]
     pub http_addr: String,
-
-    #[clap(long, short = 'f', default_value = "bns-node.toml")]
-    pub config_filename: String,
 
     #[clap(long, short = 'v', default_value = "Info")]
     pub log_level: String,
@@ -41,31 +34,12 @@ pub struct Args {
     pub eth_key: String,
 }
 
-async fn run(localhost: &str, key: SecretKey, stun: &str) {
+async fn run(http_addr: String, key: SecretKey, stun: &str) {
     let swarm = Swarm::new(Arc::new(AcChannel::new(1)), stun.to_string());
     let signaler = swarm.signaler();
-    let localhost = localhost.to_owned();
 
-    tokio::spawn(async move {
-        let swarm = swarm.clone();
-        let http_addr = localhost.clone();
-        let service = make_service_fn(move |_| {
-            let swarm = swarm.to_owned();
-            async move {
-                Ok::<_, hyper::Error>(service_fn(move |req| {
-                    discoveries_services(req, swarm.to_owned(), key)
-                }))
-            }
-        });
-
-        let sock_addr: SocketAddr = http_addr.clone().parse().unwrap();
-        let server = Server::bind(&sock_addr).serve(service);
-        println!("Serving on {}", http_addr.clone());
-        // Run this server for... forever!
-        if let Err(e) = server.await {
-            eprintln!("server error: {}", e);
-        }
-    });
+    let cloned_swarm = swarm.clone();
+    tokio::spawn(async move { run_service(http_addr, cloned_swarm, key).await });
 
     tokio::select! {
         _ = signaler.recv() => {
@@ -81,7 +55,10 @@ async fn run(localhost: &str, key: SecretKey, stun: &str) {
 async fn main() -> Result<()> {
     let args = Args::parse();
     Logger::init(args.log_level)?;
+
     let key = SecretKey::try_from(args.eth_key.as_str())?;
-    run(&args.http_addr, key, &args.stun_server).await;
+
+    run(args.http_addr, key, &args.stun_server).await;
+
     Ok(())
 }
---
+++ src/service/mod.rs
@@ -0,0 +1,38 @@
+mod discovery;
+mod http_error;
+mod observer;
+
+use axum::{
+    routing::{get, post},
+    AddExtensionLayer, Router, Server,
+};
+use bns_core::signing::SecretKey;
+use bns_core::swarm::Swarm;
+
+pub async fn run_service(addr: String, swarm: Swarm, key: SecretKey) {
+    let binding_addr = addr.parse().unwrap();
+
+    let swarm_layer = AddExtensionLayer::new(swarm);
+    let key_layer = AddExtensionLayer::new(key);
+
+    let app = Router::new()
+        .route(
+            "/sdp",
+            post(discovery::handshake_handler)
+                .layer(&swarm_layer)
+                .layer(&key_layer),
+        )
+        .route(
+            "/connect",
+            get(discovery::connect_handler)
+                .layer(&swarm_layer)
+                .layer(&key_layer),
+        );
+
+    println!("Service listening on http://{}", addr);
+
+    Server::bind(&binding_addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
