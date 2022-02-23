03b090f9f31925e7efb73d87a4e01082a670e83f
Ryan.K <ryankung@ieee.org>
2022-02-23 14:57:20 +0800

    bugfix and unittest

--- bns-core/src/dht/chord.rs
+++ bns-core/src/dht/chord.rs
@@ -1,22 +1,24 @@
 use crate::did::Did;
-use num_bigint::BigUint;
-use anyhow::Result;
 use anyhow::anyhow;
+use anyhow::Result;
+use num_bigint::BigUint;
 
+#[derive(Clone, Debug, PartialEq)]
 pub enum FoundRes {
     Found(Did),
-    ToFind(Did)
+    ToFind(Did),
 }
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 pub struct Chord {
     // ﬁrst node on circle that succeeds (n + 2 ^(k-1) ) mod 2^m , 1 <= k<= m
+    // for index start with 0, it should be (n+2^k) mod 2^m
     pub finger: Vec<Option<Did>>,
     // The next node on the identiﬁer circle; ﬁnger[1].node
     pub successor: Option<Did>,
     // The previous node on the identiﬁer circle
     pub predecessor: Option<Did>,
-    pub id: Did
+    pub id: Did,
 }
 
 impl Chord {
@@ -26,22 +28,33 @@ impl Chord {
             predecessor: None,
             // for Eth idess, it's 160
             finger: vec![None; 160],
-            id
+            id,
         }
     }
 
     pub fn join(&mut self, id: Did) {
         for k in 0u32..160u32 {
             let n: BigUint = self.id.into();
-            if (n + BigUint::from(2u16).pow(k - 1)) % BigUint::from(2u16).pow(160) <= id.into() {
-                self.finger[k as usize] = Some(id)
+            // (n + 2^k) % 2^m >= n
+            let delta = (n + BigUint::from(2u16).pow(k)) % BigUint::from(2u16).pow(160);
+            if delta <= id.into() {
+                match self.finger[k as usize] {
+                    Some(v) => {
+                        if delta > v.into() {
+                            self.finger[k as usize] = Some(id);
+                        }
+                    }
+                    None => {
+                        self.finger[k as usize] = Some(id);
+                    }
+                }
             }
         }
         self.successor = self.finger[0];
     }
 
     pub fn cloest_precding_node(&self, id: Did) -> Result<Did> {
-        for i in (1 .. 160).rev() {
+        for i in (1..160).rev() {
             if let Some(t) = self.finger[i] {
                 if t > self.id && t < id {
                     return Ok(t);
@@ -51,20 +64,53 @@ impl Chord {
         Err(anyhow!("cannot find cloest precding node"))
     }
 
+    // Fig.5 n.find_successor(id)
     pub fn find_successor(&self, id: Did) -> Result<FoundRes> {
         match self.successor {
             Some(successor) => {
+                // if (id \in (n; successor]); return successor
                 if self.id < id && id <= successor {
                     Ok(FoundRes::Found(id))
                 } else {
+                    // n = closest preceding node(id);
+                    // return n.ﬁnd_successor(id);
                     match self.cloest_precding_node(id) {
                         Ok(n) => Ok(FoundRes::ToFind(n)),
-                        Err(e) => Err(anyhow!(e))
+                        Err(e) => Err(anyhow!(e)),
                     }
-
                 }
-            },
-            None => Err(anyhow!("successor not found"))
+            }
+            None => Err(anyhow!("successor not found")),
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::str::FromStr;
+
+    #[test]
+    fn test_chord() {
+        let a = Did::from_str("0x11E807fcc88dD319270493fB2e822e388Fe36ab0").unwrap();
+        let b = Did::from_str("0x999999cf1046e68e36E1aA2E0E07105eDDD1f08E").unwrap();
+        let c = Did::from_str("0xc0ffee254729296a45a3885639AC7E10F9d54979").unwrap();
+
+        let mut node_a = Chord::new(a);
+        let mut node_b = Chord::new(b);
+        let mut node_c = Chord::new(c);
+
+        assert!(a < b && b < c);
+        node_a.join(b);
+        assert_eq!(node_a.successor.unwrap(), b);
+        assert_eq!(node_a.find_successor(b).unwrap(), FoundRes::Found(b));
+        assert_eq!(node_a.find_successor(c).unwrap(), FoundRes::ToFind(b));
+        node_a.join(c);
+        assert_eq!(node_a.successor.unwrap(), b, "{:?}", node_a.finger);
+        node_b.join(c);
+        // because a < b < c
+        // c not in (a, successor)
+        // go find cloest_precding_node which id in (a, c)
+        assert_eq!(node_a.find_successor(c).unwrap(), FoundRes::ToFind(b));
     }
 }
--- bns-core/src/did.rs
+++ bns-core/src/did.rs
@@ -1,8 +1,9 @@
 use num_bigint::BigUint;
 use std::ops::Deref;
+use std::str::FromStr;
 use web3::types::Address;
 
-#[derive(Copy, Clone, Eq, Ord, PartialEq, PartialOrd)]
+#[derive(Copy, Clone, Eq, Ord, PartialEq, PartialOrd, Debug)]
 pub struct Did(Address);
 
 impl Deref for Did {
@@ -21,5 +22,26 @@ impl From<Did> for BigUint {
 impl From<Address> for Did {
     fn from(addr: Address) -> Self {
         Self(addr)
+    }
+}
+
+impl FromStr for Did {
+    type Err = anyhow::Error;
+    fn from_str(s: &str) -> anyhow::Result<Self> {
+        Ok(Self(Address::from_str(&s)?.into()))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::str::FromStr;
+
+    #[test]
+    fn test_did() {
+        let a = Did::from_str("0x11E807fcc88dD319270493fB2e822e388Fe36ab0").unwrap();
+        let b = Did::from_str("0x999999cf1046e68e36E1aA2E0E07105eDDD1f08E").unwrap();
+        let c = Did::from_str("0xc0ffee254729296a45a3885639AC7E10F9d54979").unwrap();
+        assert!(c > b && b > a);
     }
 }
