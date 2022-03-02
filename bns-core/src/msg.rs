b44f34268b86d4ee4719dca415fe194120bc5c57
Ryan.K <ryankung@ieee.org>
2022-03-03 06:20:25 +0800

    cargo fmt

--- bns-core/src/msg.rs
+++ bns-core/src/msg.rs
@@ -1,8 +1,8 @@
 use crate::ecc::{sign, verify, SecretKey};
 use crate::encoder::Encoded;
-use chrono::Utc;
 use anyhow::anyhow;
 use anyhow::Result;
+use chrono::Utc;
 use serde::de::DeserializeOwned;
 use serde::Deserialize;
 use serde::Serialize;
