5e5107f9702a0848ec0841eda71320a24de7e03b
Ryan.K <ryankung@ieee.org>
2022-03-20 00:10:56 +0800

    fixed channel for wasm

--- bns-core/src/channels/wasm.rs
+++ bns-core/src/channels/wasm.rs
@@ -3,24 +3,28 @@ use crate::types::channel::Event;
 /// ref: https://github.com/Ciantic/rust-shared-wasm-experiments/blob/master/src/lib.rs
 use anyhow::Result;
 use async_trait::async_trait;
-use crossbeam_channel as cbc;
+//use crossbeam_channel as cbc;
+use futures::channel::mpsc;
+use futures::SinkExt;
+use futures::lock::Mutex;
+use std::sync::Arc;
 
-#[derive(Clone, Debug)]
+#[derive(Debug)]
 pub struct CbChannel {
-    sender: cbc::Sender<Event>,
-    receiver: cbc::Receiver<Event>,
+    sender: Arc<Mutex<mpsc::Sender<Event>>>,
+    receiver: Arc<Mutex<mpsc::Receiver<Event>>>,
 }
 
 #[async_trait(?Send)]
 impl Channel for CbChannel {
-    type Sender = cbc::Sender<Event>;
-    type Receiver = cbc::Receiver<Event>;
+    type Sender = Arc<Mutex<mpsc::Sender<Event>>>;
+    type Receiver = Arc<Mutex<mpsc::Receiver<Event>>>;
 
     fn new(buffer: usize) -> Self {
-        let (tx, rx) = cbc::bounded(buffer);
+        let (tx, rx) = mpsc::channel(buffer);
         Self {
-            sender: tx,
-            receiver: rx,
+            sender: Arc::new(Mutex::new(tx)),
+            receiver: Arc::new(Mutex::new(rx)),
         }
     }
 
@@ -33,10 +37,20 @@ impl Channel for CbChannel {
     }
 
     async fn send(&self, e: Event) -> Result<()> {
-        Ok(self.sender.send(e)?)
+        let mut sender = self.sender.lock().await;
+        Ok(sender.send(e).await?)
     }
 
     async fn recv(&self) -> Result<Event> {
-        self.receiver().recv().map_err(|e| anyhow::anyhow!(e))
+        let mut receiver = self.receiver.lock().await;
+        match receiver.try_next() {
+            Ok(Some(e)) => {
+                Ok(e)
+            },
+            Ok(None) => {
+                Err(anyhow::anyhow!("received empty msg"))
+            },
+            Err(e) => Err(anyhow::anyhow!(e))
+        }
     }
 }
