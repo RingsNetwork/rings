647ac71f296c29676fc11b9fc01d59a855c8f134
Ryan.K <ryankung@ieee.org>
2022-03-29 04:18:13 +0800

    reorg tests

---
+++ bns-core/tests/wasm/mod.rs
@@ -0,0 +1,2 @@
+pub mod test_wasm_transport;
+pub mod test_ice_servers;
---
+++ bns-core/tests/wasm/test_ice_servers.rs
@@ -0,0 +1,28 @@
+#[cfg(feature = "wasm")]
+#[cfg(test)]
+pub mod test {
+    use bns_core::types::ice_transport::ice_server::IceServer;
+    use js_sys::Array;
+    use js_sys::Reflect;
+    use std::str::FromStr;
+    use wasm_bindgen::JsCast;
+    use wasm_bindgen::JsValue;
+    use wasm_bindgen_test::wasm_bindgen_test;
+    use web_sys::RtcIceServer;
+
+    #[wasm_bindgen_test]
+    fn test_transform() {
+        let c = "turn://ryan@ethereum.org:9090/nginx/v2";
+        let ret_a: RtcIceServer = IceServer::from_str(c).unwrap().into();
+        let ret_urls = Reflect::get(&ret_a, &JsValue::from_str("urls")).unwrap();
+        let urls: &Array = ret_urls.as_ref().unchecked_ref();
+        assert_eq!(
+            urls.get(0),
+            JsValue::from_str("turn:ethereum.org:9090/nginx/v2")
+        );
+        assert_eq!(urls.get(1), JsValue::UNDEFINED);
+
+        let username = Reflect::get(&ret_a, &JsValue::from_str("username")).unwrap();
+        assert_eq!(username, JsValue::from_str("ryan"));
+    }
+}
