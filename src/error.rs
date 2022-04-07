c03883c8d1b6db700da55bea6d4a0083d2ba7223
momo <googolmo@gmail.com>
2022-04-07 17:19:35 +0800

    use jsonrpc for node backend server

---
+++ src/error.rs
@@ -0,0 +1,68 @@
+pub type Result<T> = std::result::Result<T, Error>;
+
+#[derive(Debug, thiserror::Error)]
+pub enum Error {
+    #[error("Connect remote rpc server failed: {0}.")]
+    RemoteRpcError(String),
+    #[error("Pending Transport error: {0}.")]
+    PendingTransport(bns_core::err::Error),
+    #[error("Transport not found.")]
+    TransportNotFound,
+    #[error("Create Transport error.")]
+    NewTransportError,
+    #[error("Close Transport error: {0}.")]
+    CloseTransportError(bns_core::err::Error),
+    #[error("Decode error.")]
+    DecodedError,
+    #[error("Encode error.")]
+    EncodedError,
+    #[error("Register ICE error: {0}.")]
+    RegisterIceError(bns_core::err::Error),
+    #[error("Create offer info failed: {0}.")]
+    CreateOffer(bns_core::err::Error),
+    #[error("Create answer info failed: {0}.")]
+    CreateAnswer(bns_core::err::Error),
+    #[error("Invalid transport id.")]
+    InvalidTransportId,
+    #[error("Invalid address.")]
+    InvalidAddress,
+    #[error("Json serialize error.")]
+    JsonSerializeError,
+    #[error("Json Deserialize error.")]
+    JsonDeserializeError,
+    #[error("Invalid method.")]
+    InvalidMethod,
+}
+
+impl Error {
+    pub fn code(&self) -> i64 {
+        let code = match self {
+            Error::RemoteRpcError(_) => 0,
+            Error::PendingTransport(_) => 1,
+            Error::TransportNotFound => 2,
+            Error::NewTransportError => 3,
+            Error::CloseTransportError(_) => 4,
+            Error::DecodedError => 5,
+            Error::EncodedError => 6,
+            Error::RegisterIceError(_) => 7,
+            Error::CreateOffer(_) => 8,
+            Error::CreateAnswer(_) => 9,
+            Error::InvalidTransportId => 10,
+            Error::InvalidAddress => 11,
+            Error::JsonSerializeError => 12,
+            Error::JsonDeserializeError => 13,
+            Error::InvalidMethod => 14,
+        };
+        -32000 - code
+    }
+}
+
+impl From<Error> for jsonrpc_core::Error {
+    fn from(e: Error) -> Self {
+        Self {
+            code: jsonrpc_core::ErrorCode::ServerError(e.code()),
+            message: e.to_string(),
+            data: None,
+        }
+    }
+}
---
+++ src/service/jsonrpc.rs
@@ -0,0 +1,66 @@
+use super::request::Method;
+use crate::{error::Error as ServerError, service::processor::Processor};
+use jsonrpc_core::{Error, ErrorCode, MetaIoHandler, Params, Result, Value};
+
+pub async fn build_handler(handler: &mut MetaIoHandler<Processor>) {
+    handler.add_method_with_meta(Method::ConnectPeerViaHttp.as_str(), connect_peer_via_http);
+    handler.add_method_with_meta(Method::ConnectPeerViaIce.as_str(), connect_peer_via_ice);
+    handler.add_method_with_meta(Method::CreateOffer.as_str(), create_offer);
+    handler.add_method_with_meta(Method::AcceptAnswer.as_str(), accept_answer);
+    handler.add_method_with_meta(Method::ListPeers.as_str(), list_peers);
+    handler.add_method_with_meta(Method::Disconnect.as_str(), close_connection);
+}
+
+pub async fn connect_peer_via_http(params: Params, processor: Processor) -> Result<Value> {
+    let p: Vec<String> = params.parse()?;
+    let peer_url = p
+        .first()
+        .ok_or_else(|| Error::new(ErrorCode::InvalidParams))?;
+    let id = processor
+        .connect_peer_via_http(peer_url)
+        .await
+        .map_err(Error::from)?;
+    Ok(Value::String(id))
+}
+
+pub async fn connect_peer_via_ice(params: Params, processor: Processor) -> Result<Value> {
+    let p: Vec<String> = params.parse()?;
+    let ice_info = p
+        .first()
+        .ok_or_else(|| Error::new(ErrorCode::InvalidParams))?;
+    let r = processor
+        .connect_peer_via_ice(ice_info)
+        .await
+        .map_err(Error::from)?;
+    r.to_json_obj().map_err(Error::from)
+}
+
+pub async fn create_offer(_params: Params, processor: Processor) -> Result<Value> {
+    let r = processor.create_offer().await.map_err(Error::from)?;
+    r.to_json_obj().map_err(Error::from)
+}
+
+pub async fn accept_answer(params: Params, processor: Processor) -> Result<Value> {
+    let params: Vec<String> = params.parse()?;
+    if let ([transport_id, ice], _) = params.split_at(2) {
+        let r = processor
+            .accept_answer(transport_id.as_str(), ice.as_str())
+            .await?;
+        return r.to_json_obj().map_err(Error::from);
+    };
+    Err(Error::new(ErrorCode::InvalidParams))
+}
+
+pub async fn list_peers(_params: Params, processor: Processor) -> Result<Value> {
+    let r = processor.list_peers().await?;
+    Ok(serde_json::to_value(&r).map_err(|_| Error::from(ServerError::JsonSerializeError))?)
+}
+
+pub async fn close_connection(params: Params, processor: Processor) -> Result<Value> {
+    let params: Vec<String> = params.parse()?;
+    let address = params
+        .first()
+        .ok_or_else(|| Error::new(ErrorCode::InvalidParams))?;
+    processor.disconnect(address).await?;
+    Ok(serde_json::json!({}))
+}
