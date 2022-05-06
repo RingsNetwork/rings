d3cb55f0faad425ab2020dafc12b7542c98a0ef3
Ryan.K <ryankung@ieee.org>
2022-05-07 02:29:36 +0800

    rm unstable files

--- rings-core/src/message/handlers/handler_ng.rs
+++
@@ -1,6 +0,0 @@
-// use crate::dht::{Did, PeerRing};
-// use crate::err::{Error, Result};
-// use crate::message::payload_ng::{MessageRelay, Message, MsgSender};
-// use crate::message::types::Message;
-// use crate::swarm::Swarm;
-// use async_trait::async_trait;
