3bc68e7a949b87484c553beb6b0ffd36d4b4ea7e
Ryan.K <ryankung@ieee.org>
2022-05-12 19:52:12 +0800

    needs to rewrite test

--- rings-core/src/dht/peer.rs
+++ rings-core/src/dht/peer.rs
@@ -3,10 +3,13 @@ use crate::err::{Error, Result};
 use crate::message::Encoded;
 use crate::message::Encoder;
 use crate::message::MessageRelay;
+use crate::ecc::HashStr;
 use num_bigint::BigUint;
 use serde::de::DeserializeOwned;
 use serde::Deserialize;
 use serde::Serialize;
+
+use std::str::FromStr;
 
 #[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
 pub struct VirtualPeer {
@@ -31,6 +34,17 @@ where
         Ok(Self {
             address: address.into(),
             data: vec![data],
+        })
+    }
+}
+
+impl TryFrom<Encoded> for VirtualPeer {
+    type Error = Error;
+    fn try_from(e: Encoded) -> Result<Self> {
+        let address: HashStr = e.value().into();
+        Ok(Self {
+            address: Did::from_str(&address.inner())?,
+            data: vec!(e)
         })
     }
 }
