6a6124788b74096db86c0db6156caf4570c94f78
magine <magine233@gmail.com>
2022-06-06 21:38:12 +0800

    add docs for relay protocol

--- rings-core/src/message/protocol.rs
+++ rings-core/src/message/protocol.rs
@@ -1,3 +1,35 @@
+//! # How relay works
+//!
+//! By calling `relay` method in correct place, relay help to do things:
+//! - Infer the next_hop of a message.
+//! - Get the sender and origin sender of a message.
+//! - Record the whole transport path for inspection.
+//!
+//! Relay divides messages into two types by method: SEND and REPORT.
+//! Generally, a node that got a SEND message will either transpond it to another node or respond with a REPORT message.
+//! And the responded REPORT message will be sent back to the original sender (by inversed came path if possible).
+//!
+//! All messages should be payload with relay, which consists of the following fields:
+//! - `method`: The method of message. SEND or REPORT.
+//! - `path`: A push only stack. Record routes when handling sending messages.
+//! - `path_end_cursor`: Move this cursor to flag the top of the stack when reporting.
+//!    Notice that this cursor is not the index of current.
+//!    It's `path.len() - <index of current> - 1`, which means count down to head of vector.
+//!    It will always be 0 while handling sending messages in this way.
+//! - `next_hop`: The next node to handle the message.
+//!   When and only when located at the end of the message propagation, the `next_hop` is none.
+//!   The current handler will pick transport by this field.
+//! - `destination`: The destination of the message. It may be customized when sending. It cannot be changed when reporting.
+//!   It may help the handler to find out `next_hop` in some situations.
+//!
+//! When handling a SEND message, the `relay(&mut self, current: Did, next_hop: Option<Did>)` method
+//! push `current` to the `self.path` stack, and set `next_hop` parameter to `self.next_node`.
+//!
+//! When handling a REPORT message, the `relay(&mut self, current: Did, next_hop: Option<Did>)` method
+//! will move forward `self.path_end_cursor` to the position of `current` in `self.path`.
+//! If `next_hop` parameter is none, it will also pick the previous node in `self.path` as `self.next_hop`.
+//! (With this feature, one can always pass None as `next_hop` parameter when handling a REPORT message.)
+
 #![warn(missing_docs)]
 
 use super::payload::MessageRelay;
@@ -126,6 +158,42 @@ mod test {
     use super::*;
     use crate::ecc::SecretKey;
     use crate::message::payload::test::new_test_message;
+    use crate::session::SessionManager;
+
+    #[test]
+    fn test_path_end_cursor() {
+        let mut send_payload = new_test_message();
+        let next_hop1 = SecretKey::random().address().into();
+        let next_hop2 = SecretKey::random().address().into();
+        let next_hop3 = SecretKey::random().address().into();
+
+        // node0 -> node1
+        send_payload.relay(next_hop1, None).unwrap();
+        assert_eq!(send_payload.path_end_cursor, 0);
+
+        // node0 -> node1 -> node2
+        send_payload.relay(next_hop2, None).unwrap();
+        assert_eq!(send_payload.path_end_cursor, 0);
+
+        // node0 -> node1 -> node2 -> node3
+        send_payload.relay(next_hop3, None).unwrap();
+        assert_eq!(send_payload.path_end_cursor, 0);
+
+        // node3 make REPORT, destination is node0
+        let fake_session = SessionManager::new_with_seckey(&SecretKey::random()).unwrap();
+        let mut report_payload = send_payload
+            .report(send_payload.data.clone(), &fake_session)
+            .unwrap();
+        assert_eq!(report_payload.path_end_cursor, 0);
+
+        // node0 -> node1 -> node2 -> node3 -> node2
+        report_payload.relay(next_hop2, None).unwrap();
+        assert_eq!(report_payload.path_end_cursor, 1);
+
+        // node0 -> node1 -> node2 -> node3 -> node2 -> node1
+        report_payload.relay(next_hop1, None).unwrap();
+        assert_eq!(report_payload.path_end_cursor, 2);
+    }
 
     #[test]
     fn test_path_prev() {
