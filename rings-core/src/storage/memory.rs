c0e76e16b7caf493b38ef0723df5e537ab1b243c
magine <magine233@gmail.com>
2022-06-08 23:25:02 +0800

    Transport manager do not need persistent storage

--- rings-core/src/storage/memory.rs
+++ rings-core/src/storage/memory.rs
@@ -1,63 +1,55 @@
-use crate::storage::Storage;
 use dashmap::DashMap;
 use std::hash::Hash;
 
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, Default)]
 pub struct MemStorage<K, V>
 where
-    K: Clone + Eq + Hash,
+    K: Copy + Eq + Hash,
     V: Clone,
 {
     table: DashMap<K, V>,
 }
 
-impl<K, V> Storage for MemStorage<K, V>
+impl<K, V> MemStorage<K, V>
 where
-    K: Clone + Eq + Hash,
+    K: Copy + Eq + Hash,
     V: Clone,
 {
-    type K = K;
-    type V = V;
-
-    fn new() -> Self {
+    pub fn new() -> Self {
         Self {
-            table: DashMap::new(),
+            table: DashMap::default(),
         }
     }
 
-    fn get(&self, addr: &K) -> Option<V> {
+    pub fn get(&self, addr: &K) -> Option<V> {
         self.table.get(addr).map(|v| v.value().clone())
     }
 
-    fn set(&self, addr: &K, value: V) -> Option<V> {
-        self.table.insert(addr.clone(), value)
+    pub fn set(&self, addr: &K, value: V) -> Option<V> {
+        self.table.insert(*addr, value)
     }
 
-    fn len(&self) -> usize {
+    pub fn len(&self) -> usize {
         self.table.len()
     }
 
-    fn is_empty(&self) -> bool {
+    pub fn is_empty(&self) -> bool {
         self.table.len() == 0
     }
 
-    fn get_or_set(&self, addr: &Self::K, default: Self::V) -> Self::V {
-        self.table
-            .entry(addr.clone())
-            .or_insert(default)
-            .value()
-            .clone()
+    pub fn get_or_set(&self, addr: &K, default: V) -> V {
+        self.table.entry(*addr).or_insert(default).value().clone()
     }
 
-    fn keys(&self) -> Vec<Self::K> {
+    pub fn keys(&self) -> Vec<K> {
         self.table.clone().into_iter().map(|(k, _v)| k).collect()
     }
 
-    fn values(&self) -> Vec<Self::V> {
+    pub fn values(&self) -> Vec<V> {
         self.table.clone().into_iter().map(|(_k, v)| v).collect()
     }
 
-    fn items(&self) -> Vec<(Self::K, Self::V)> {
+    pub fn items(&self) -> Vec<(K, V)> {
         self.table
             .clone()
             .into_iter()
@@ -65,10 +57,34 @@ where
             .collect()
     }
 
-    fn remove(&self, addr: &Self::K) -> Option<(Self::K, Self::V)> {
+    pub fn remove(&self, addr: &K) -> Option<(K, V)> {
         match self.get(addr) {
             Some(_) => self.table.remove(addr),
             None => None,
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::ecc::SecretKey;
+    use web3::types::Address;
+
+    #[test]
+    fn memstorage_basic_interface_should_work() {
+        let store = MemStorage::<Address, String>::new();
+        let addr = SecretKey::random().address();
+
+        assert!(store.get(&addr).is_none());
+
+        let v0 = store.set(&addr, "value 1".into());
+        assert!(v0.is_none());
+
+        let v1 = store.set(&addr, "value 2".into());
+        assert_eq!(v1, Some("value 1".into()));
+
+        let v2 = store.get(&addr);
+        assert_eq!(v2, Some("value 2".into()));
     }
 }
