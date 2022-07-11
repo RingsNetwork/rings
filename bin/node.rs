86f8d9f01755f65fe6c7893e1cf684030cdf90d4
Elder Ryan <ryankung@ieee.org>
2022-07-12 01:47:09 +0800

    Features/some doc (#212)
    * rename to rings-clie/rings-daemon
    * added about
    * fmt
    * version 0.1.3

--- bin/node.rs
+++ bin/node.rs
@@ -59,7 +59,7 @@ struct Daemon {
     pub ice_servers: String,
 
     #[clap(long = "key", short = 'k', env)]
-    pub eth_key: SecretKey,
+    pub ecdsa_key: SecretKey,
 
     #[clap(long, default_value = "20", env)]
     pub stabilize_timeout: usize,
@@ -93,7 +93,7 @@ enum ConnectCommand {
 }
 
 #[derive(Args, Debug)]
-#[clap(about)]
+#[clap(about = "Connect with Node url")]
 struct ConnectUrlArgs {
     #[clap(flatten)]
     client_args: ClientArgs,
@@ -103,7 +103,7 @@ struct ConnectUrlArgs {
 }
 
 #[derive(Args, Debug)]
-#[clap(about)]
+#[clap(about = "Connect with Did address via DHT")]
 struct ConnectWithAddressArgs {
     #[clap(flatten)]
     client_args: ClientArgs,
@@ -127,7 +127,7 @@ enum SdpCommand {
 #[clap(about)]
 struct SdpOffer {
     #[clap(long = "key", short = 'k', env)]
-    pub eth_key: SecretKey,
+    pub ecdsa_key: SecretKey,
     #[clap(
         long,
         short = 's',
@@ -256,7 +256,7 @@ async fn main() -> anyhow::Result<()> {
         Command::Run(args) => {
             daemon_run(
                 args.http_addr,
-                &args.eth_key,
+                &args.ecdsa_key,
                 args.ice_servers.as_str(),
                 args.stabilize_timeout,
             )
