78020bd9a4da0275c2d784b7082638e5c5e82cf5
Ryan.K <ryankung@ieee.org>
2022-08-08 19:44:48 +0800

    added test

--- rings-core/src/ecc/signers.rs
+++ rings-core/src/ecc/signers.rs
@@ -69,19 +69,21 @@ pub mod eip712 {
 }
 
 pub mod ed25519 {
+    use ed25519_dalek::Verifier;
+
     use super::*;
 
     pub fn verify(msg: &str, address: &Address, sig: impl AsRef<[u8]>, pubkey: PublicKey) -> bool {
-        if sig.as_ref().len() != 64 {
+        if pubkey.address() != *address {
             return false;
         }
-        if pubkey.address() != *address {
+        if sig.as_ref().len() != 64 {
             return false;
         }
         let sig_data: [u8; 64] = sig.as_ref().try_into().unwrap();
         let sig = ed25519_dalek::Signature::new(sig_data);
         if let Ok(p) = TryInto::<ed25519_dalek::PublicKey>::try_into(pubkey) {
-            match p.verify_strict(msg.as_bytes(), &sig) {
+            match p.verify(msg.as_bytes(), &sig) {
                 Ok(()) => true,
                 Err(_) => false,
             }
@@ -147,7 +149,7 @@ mod test {
         let msg = "helloworld";
         let signer =
             PublicKey::try_from_b58t("9z1ZTaGocNSAu3DSqGKR6Dqt214X4dXucVd6C53EgqBK").unwrap();
-        let sig_b58 = "3BfW8GwZ5QKi9txfsf2wNTe7ksoEzbHW4LrpPTheR5cms4XBMm84pFvWMZ4rxfj8jNJesqnZuBjP5e9y2Um13ccU";
+        let sig_b58 = "2V1AR5byk4a4CkVmFRWU1TVs3ns2CGkuq6xgGju1huGQGq5hGkiHUDjEaJJaL2txfqCSGnQW55jUJpcjKFkZEKq";
         let sig: Vec<u8> = base58::FromBase58::from_base58(sig_b58).unwrap();
         assert!(ed25519::verify(
             msg,
