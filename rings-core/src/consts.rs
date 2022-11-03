68449c05ce45f1b71b032ae2b79a48c448feba74
magine <magine233@gmail.com>
2022-11-03 15:26:19 +0800

    ttl safety

--- rings-core/src/consts.rs
+++ rings-core/src/consts.rs
@@ -2,6 +2,8 @@
 ///
 /// default ttl in ms
 pub const DEFAULT_TTL_MS: usize = 300 * 1000;
+pub const MAX_TTL_MS: usize = DEFAULT_TTL_MS * 10;
+pub const TS_OFFSET_TOLERANCE_MS: u128 = 3000;
 pub const DEFAULT_SESSION_TTL_MS: usize = 30 * 24 * 3600 * 1000;
 pub const TRANSPORT_MTU: usize = 60000;
 pub const TRANSPORT_MAX_SIZE: usize = TRANSPORT_MTU * 16;
