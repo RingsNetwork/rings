b26608da6075d8171459594e7bac7952af71a6e1
magine <magine233@gmail.com>
2023-03-21 19:49:22 +0800

    Show node did in status api

--- node/src/processor.rs
+++ node/src/processor.rs
@@ -59,6 +59,8 @@ use crate::prelude::Signer;
 pub struct NodeInfo {
     /// node version
     pub version: String,
+    /// node did
+    pub did: String,
     /// swarm inspect info
     pub swarm: SwarmInspect,
 }
@@ -639,6 +641,7 @@ impl Processor {
     pub async fn get_node_info(&self) -> Result<NodeInfo> {
         Ok(NodeInfo {
             version: crate::util::build_version(),
+            did: self.swarm.did().to_string(),
             swarm: self.swarm.inspect().await,
         })
     }
