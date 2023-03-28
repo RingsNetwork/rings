1b72fb995ea6522cffe24b2d374813557f95ccd5
Elder Ryan <ryankung@ieee.org>
2023-03-28 23:02:42 +0800

    dummy support random delay (#380)

--- core/src/transports/dummy/mod.rs
+++ core/src/transports/dummy/mod.rs
@@ -1,10 +1,24 @@
+mod consts;
 /// A dummy transport use for test.
 pub mod transport;
 
+use rand::distributions::Distribution;
+use tokio::time::sleep;
+use tokio::time::Duration;
 pub use transport::DummyTransport;
 use webrtc::ice_transport::ice_candidate::RTCIceCandidateInit;
 
 use crate::types::ice_transport::IceCandidate;
+
+fn random() -> u64 {
+    let range = rand::distributions::Uniform::new(consts::DUMMY_DELAY_MIN, consts::DUMMY_DELAY_MAX);
+    let mut rng = rand::thread_rng();
+    range.sample(&mut rng)
+}
+
+async fn random_delay() {
+    sleep(Duration::from_millis(random())).await;
+}
 
 impl From<RTCIceCandidateInit> for IceCandidate {
     fn from(cand: RTCIceCandidateInit) -> Self {
