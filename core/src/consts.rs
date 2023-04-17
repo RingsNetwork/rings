ded1cdde4ee6b93535043116ffa5d027cbea3c43
Ryan.K <ryankung@ieee.org>
2023-04-17 23:08:21 +0800

    added new feature

--- core/src/consts.rs
+++ core/src/consts.rs
@@ -8,4 +8,8 @@ pub const DEFAULT_SESSION_TTL_MS: usize = 30 * 24 * 3600 * 1000;
 pub const TRANSPORT_MTU: usize = 60000;
 pub const TRANSPORT_MAX_SIZE: usize = TRANSPORT_MTU * 16;
 pub const VNODE_DATA_MAX_LEN: usize = 1024;
+
+#[cfg(feature = "redundant")]
+pub const VNODE_DATA_REDUNDANT: u16 = 6;
+#[cfg(not(feature = "redundant"))]
 pub const VNODE_DATA_REDUNDANT: u16 = 1;
