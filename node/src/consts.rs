f4acafa09cfb818af892b3e6e66a6cf39305078e
Ryan.K <ryankung@ieee.org>
2023-05-18 15:50:25 +0800

    set data redundant

--- node/src/consts.rs
+++ node/src/consts.rs
@@ -2,4 +2,4 @@ use crate::prelude::rings_core::consts::*;
 
 pub const BACKEND_MTU: usize = TRANSPORT_MAX_SIZE - TRANSPORT_MTU;
 /// Redundant setting of vnode data storage
-pub const DATA_REDUNDANT: u16 = 1;
+pub const DATA_REDUNDANT: u16 = 6;
