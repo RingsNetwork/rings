1470752b115453505684c677a85be78480c29837
Ryan.K <ryankung@ieee.org>
2023-05-26 22:01:45 +0800

    fmt

--- derive/src/lib.rs
+++ derive/src/lib.rs
@@ -1,16 +1,16 @@
 extern crate proc_macro;
 use proc_macro::TokenStream;
 
-/// A proc macro that do nothing
+/// If the feature is "wasm", the macro does nothing; otherwise, it calls `wasm_bindgen`.
 #[proc_macro_attribute]
 pub fn wasm_export(_attr: TokenStream, input: TokenStream) -> TokenStream {
 
-    #[cfg(features="wasm")]
+    #[cfg(features = "wasm")]
     {
-	return rings_core::prelude::wasm_bindgen::wasm_bindgen(_attr, input);
+        return rings_core::prelude::wasm_bindgen::wasm_bindgen(_attr, input);
     }
 
     {
-	return input;
+        return input;
     }
 }
