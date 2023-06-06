c5640e2d031ddb76619d4da63e8dc6527d9463db
Elder Ryan <ryankung@ieee.org>
2023-06-06 19:17:45 +0800

    update readme (#429)

--- derive/src/lib.rs
+++ derive/src/lib.rs
@@ -6,7 +6,7 @@ use quote::quote;
 /// If the feature is not "wasm", the macro does nothing; otherwise, it calls wasm_bindgen.
 /// wasm_export does not work for Js Class. To export a class to js,
 /// you should use wasm_bindgen or __wasm_bindgen_class_marker.
-/// ref: https://docs.rs/wasm-bindgen-macro/0.2.86/src/wasm_bindgen_macro/lib.rs.html#51
+/// ref: <https://docs.rs/wasm-bindgen-macro/0.2.86/src/wasm_bindgen_macro/lib.rs.html#51>
 #[proc_macro_attribute]
 pub fn wasm_export(attr: TokenStream, input: TokenStream) -> TokenStream {
     if !attr.is_empty() {
