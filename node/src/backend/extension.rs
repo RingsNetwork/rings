b3f7c867b65c903e0ee9a9073c6668d83220c1ed
Ryan.K <ryankung@ieee.org>
2023-06-11 03:03:11 +0800

    added document and abi

--- node/src/backend/extension.rs
+++ node/src/backend/extension.rs
@@ -1,3 +1,38 @@
+#![warn(missing_docs)]
+//! The "External" module supports a user-defined message handler based on WebAssembly (Wasm).
+//! The Rings network allows loading a Wasm or Wat file from a remote or local file and transforming it into a message handler.
+//! The Wasm module should satisfy the following requirements:
+//!
+//! 1. It should have a function with the signature fn handler(param: ExternRef) -> ExternRef, and this function should be exported.
+//!
+//! 2. The Wasm module should not have any external imports, except for the helper functions defined by the Rings network.
+//!
+//! 3. Only the helper functions defined by the Rings network can be used, which include:
+//!
+//! ```text
+//!     "message_abi" => {
+//!         "message_type"  => msg_type,
+//!         "extra" => extra,
+//!          "data" => data
+//!    }
+//! ```
+//! A basic wasm extension may looks like:
+//!
+//! ```wat
+//! (module
+//!  ;; Let's import message_type from message_abi
+//!  (type $ty_message_type (func (param externref) (result i32)))
+//!  (import "message_abi" "message_type" (func $message_type (type $ty_message_type)))
+//!   ;; fn handler(param: ExternRef) -> ExternRef
+//!  (func $handler  (param externref) (result externref)
+//!      (return (local.get 0))
+//!  )
+//!  (export "handler" (func $handler))
+//! )
+//!
+//! You can see that this wat/wasm extension defines a handler function and
+//! imported the message_type ABI.
+
 use loader::Handler;
 use serde::Deserialize;
 use serde::Serialize;
@@ -21,6 +56,7 @@ pub enum Path {
 /// Configure for Extension
 #[derive(Deserialize, Serialize, Debug, Clone, Default)]
 pub struct ExtensionConfig {
+    /// Path of extension, can be remote or local
     pub paths: Vec<Path>,
 }
 
@@ -32,6 +68,7 @@ pub struct Extension {
 
 /// Calls the extension handler with the given message and returns the response.
 pub trait ExtensionHandlerCaller {
+    /// Call extension handler
     fn call(&self, msg: BackendMessage) -> Result<BackendMessage>;
 }
 
@@ -101,6 +138,7 @@ impl MessageEndpoint for Extension {
     }
 }
 
+/// Loader of wasm, including ABI generator
 pub mod loader {
     use std::fs;
     use std::sync::Arc;
@@ -108,9 +146,13 @@ pub mod loader {
 
     use lazy_static::lazy_static;
     use wasmer::imports;
+    use wasmer::AsStoreMut;
     use wasmer::ExternRef;
     use wasmer::FromToNativeWasmType;
+    use wasmer::FunctionType;
+    use wasmer::Type;
     use wasmer::TypedFunction;
+    use wasmer::Value;
 
     use super::MaybeBackendMessage;
     use crate::backend::types::BackendMessage;
@@ -120,6 +162,101 @@ pub mod loader {
     lazy_static! {
         static ref WASM_MEM: Arc<Mutex<wasmer::Store>> =
             Arc::new(Mutex::new(wasmer::Store::default()));
+    }
+
+    /// The "WasmABILander" defines how a Rust native struct generates the corresponding Wasm ABI for its getter functions.
+    pub trait WasmABILander {
+        /// The land_abi function needs to return an ImportObject.
+        /// read more: <https://developer.mozilla.org/en-US/docs/WebAssembly/JavaScript_interface/instantiate>
+        fn land_abi(store: &mut impl AsStoreMut) -> wasmer::Imports;
+    }
+
+    impl WasmABILander for MaybeBackendMessage {
+        fn land_abi(store: &mut impl AsStoreMut) -> wasmer::Imports {
+            let msg_type = wasmer::Function::new(
+                store,
+                FunctionType::new(vec![Type::ExternRef], vec![Type::I32]),
+                MaybeBackendMessage::msg_type,
+            );
+            let extra = wasmer::Function::new(
+                store,
+                FunctionType::new(vec![Type::ExternRef], vec![Type::I32]),
+                MaybeBackendMessage::extra,
+            );
+            let data = wasmer::Function::new(
+                store,
+                FunctionType::new(vec![Type::ExternRef], vec![Type::I32]),
+                MaybeBackendMessage::extra,
+            );
+
+            imports! {
+		"message_abi" => {
+                    "message_type"  => msg_type,
+                    "extra" => extra,
+                    "data" => data
+		}
+            }
+        }
+    }
+
+    impl MaybeBackendMessage {
+        /// wasm function type `Fn (Option<ExternalRef>) -> I32`
+        pub fn msg_type(v: &[Value]) -> core::result::Result<Vec<Value>, wasmer::RuntimeError> {
+            match v {
+                [Value::ExternRef(e)] => {
+                    let maybe_msg: MaybeBackendMessage =
+                        MaybeBackendMessage::from_native(e.clone());
+                    if let Some(msg) = maybe_msg.0 {
+                        let ty: i32 = msg.message_type.into();
+                        Ok(vec![Value::I32(ty)])
+                    } else {
+                        Err(wasmer::RuntimeError::new("ExternalRef is NULL"))
+                    }
+                }
+                x => Err(wasmer::RuntimeError::new(format!(
+                    "Expect Externef, got {:?}",
+                    x
+                ))),
+            }
+        }
+        /// wasm function type `Fn (Option<ExternalRef>) -> [I32; 30]`
+        pub fn extra(v: &[Value]) -> core::result::Result<Vec<Value>, wasmer::RuntimeError> {
+            match v {
+                [Value::ExternRef(e)] => {
+                    let maybe_msg: MaybeBackendMessage =
+                        MaybeBackendMessage::from_native(e.clone());
+                    if let Some(msg) = maybe_msg.0 {
+                        let extra = msg.extra.map(|e| Value::I32(e as i32)).to_vec();
+                        Ok(extra)
+                    } else {
+                        Err(wasmer::RuntimeError::new("ExternalRef is NULL"))
+                    }
+                }
+                x => Err(wasmer::RuntimeError::new(format!(
+                    "Expect Externef, got {:?}",
+                    x
+                ))),
+            }
+        }
+        /// wasm function type `Fn (Option<ExternalRef>) -> \[I32\]`
+        pub fn data(v: &[Value]) -> core::result::Result<Vec<Value>, wasmer::RuntimeError> {
+            match v {
+                [Value::ExternRef(e)] => {
+                    let maybe_msg: MaybeBackendMessage =
+                        MaybeBackendMessage::from_native(e.clone());
+                    if let Some(msg) = maybe_msg.0 {
+                        let data = msg.data.into_iter().map(|e| Value::I32(e as i32)).collect();
+                        Ok(data)
+                    } else {
+                        Err(wasmer::RuntimeError::new("ExternalRef is NULL"))
+                    }
+                }
+                x => Err(wasmer::RuntimeError::new(format!(
+                    "Expect Externef, got {:?}",
+                    x
+                ))),
+            }
+        }
     }
 
     unsafe impl FromToNativeWasmType for MaybeBackendMessage {
@@ -140,7 +277,10 @@ pub mod loader {
                         Self(None)
                     }
                 }
-                _ => Self(None),
+                Err(e) => {
+                    log::error!("{:?}", e);
+                    Self(None)
+                }
             }
         }
 
@@ -159,17 +299,21 @@ pub mod loader {
                         None
                     }
                 }
-                _ => None,
+                Err(e) => {
+                    log::error!("{:?}", e);
+                    None
+                }
             }
         }
     }
 
+    /// Type of message handler that the Wasm/Wat should implement
     type TyHandler = TypedFunction<Option<ExternRef>, Option<ExternRef>>;
 
-    /// Externref type handler
+    /// Externref type handler, this is a wrapper of handler function
     pub struct Handler {
+	/// wrapped function
         pub func: TyHandler,
-        pub exports: wasmer::Exports,
     }
 
     impl super::ExtensionHandlerCaller for Handler {
@@ -201,7 +345,7 @@ pub mod loader {
         let module = wasmer::Module::new(&store, &bytes)
             .map_err(|e| Error::WasmCompileError(e.to_string()))?;
         // The module doesn't import anything, so we create an empty import object.
-        let import_object = imports! {};
+        let import_object = MaybeBackendMessage::land_abi(&mut store);
         let ins = wasmer::Instance::new(&mut store, &module, &import_object)
             .map_err(|_| Error::WasmInstantiationError)?;
         let exports: wasmer::Exports = ins.exports;
@@ -211,10 +355,7 @@ pub mod loader {
             .typed(&store)
             .map_err(|_| Error::WasmExportError)?;
 
-        Ok(Handler {
-            func: handler,
-            exports,
-        })
+        Ok(Handler { func: handler })
     }
 
     /// Load wasm from filesystem
@@ -241,6 +382,31 @@ mod test {
 (module
   ;; fn handler(param: ExternRef) -> ExternRef
   (func $handler  (param externref) (result externref)
+      (return (local.get 0))
+  )
+
+  (export "handler" (func $handler))
+)
+"#;
+        let data = "hello extension";
+        let handler = load(wasm.to_string()).await.unwrap();
+        let msg = BackendMessage::from((2u16, data.as_bytes()));
+        let ret = handler.call(msg.clone()).unwrap();
+        assert_eq!(ret, msg);
+    }
+
+    #[tokio::test]
+    async fn test_complex_handler() {
+        // WAT symtax: https://github.com/WebAssembly/spec/blob/master/interpreter/README.md#s-expression-syntax
+        // Intract with mem: https://github.com/wasmerio/wasmer/blob/master/examples/memory.rs
+        let wasm = r#"
+(module
+  ;; Let's import message_type from message_abi
+  (type $ty_message_type (func (param externref) (result i32)))
+  (import "message_abi" "message_type" (func $message_type (type $ty_message_type)))
+
+  ;; fn handler(param: ExternRef) -> ExternRef
+  (func $handler  (param $input externref) (result externref)
       (return (local.get 0))
   )
 
