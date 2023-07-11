93ccca863afa6e9f37049f2f26cdc0790dab831c
Ryan.K <ryankung@ieee.org>
2023-07-11 15:27:55 +0800

    error type

--- node/src/error.rs
+++ node/src/error.rs
@@ -6,111 +6,113 @@ use crate::prelude::rings_core;
 pub type Result<T> = std::result::Result<T, Error>;
 
 /// Errors enum mapping global custom errors.
+/// The error type can be expressed in decimal, where the high decs represent
+/// the error category and the low decs represent the error type.
 #[derive(Debug, thiserror::Error)]
 #[non_exhaustive]
-#[repr(u16)]
+#[repr(u32)]
 pub enum Error {
     #[error("Connect remote rpc server failed: {0}.")]
-    RemoteRpcError(String) = 0,
+    RemoteRpcError(String) = 100,
     #[error("Unknown rpc error.")]
-    UnknownRpcError,
+    UnknownRpcError = 101,
     #[error("Pending Transport error: {0}.")]
-    PendingTransport(rings_core::error::Error),
+    PendingTransport(rings_core::error::Error) = 202,
     #[error("Transport not found.")]
-    TransportNotFound,
+    TransportNotFound = 203,
     #[error("Create Transport error: {0}.")]
-    NewTransportError(rings_core::error::Error),
+    NewTransportError(rings_core::error::Error) = 204,
     #[error("Close Transport error: {0}.")]
-    CloseTransportError(rings_core::error::Error),
-    #[error("Decode error.")]
-    DecodeError,
-    #[error("Encode error.")]
-    EncodeError,
-    #[error("WASM compile error: {0}")]
-    WasmCompileError(String),
-    #[error("BackendMessage RwLock Error")]
-    WasmBackendMessageRwLockError,
-    #[error("WASM instantiation error.")]
-    WasmInstantiationError,
-    #[error("WASM export error.")]
-    WasmExportError,
-    #[error("WASM runtime error: {0}")]
-    WasmRuntimeError(String),
-    #[error("WASM global memory mutex error.")]
-    WasmGlobalMemoryLockError,
-    #[error("WASM failed to load file.")]
-    WasmFailedToLoadFile,
-    #[error("Create offer info failed: {0}.")]
-    CreateOffer(rings_core::error::Error),
-    #[error("Answer offer info failed: {0}.")]
-    AnswerOffer(rings_core::error::Error),
-    #[error("Accept answer info failed: {0}.")]
-    AcceptAnswer(rings_core::error::Error),
+    CloseTransportError(rings_core::error::Error) = 205,
     #[error("Invalid transport id.")]
-    InvalidTransportId,
+    InvalidTransportId = 206,
+    #[error("Create offer info failed: {0}.")]
+    CreateOffer(rings_core::error::Error) = 207,
+    #[error("Answer offer info failed: {0}.")]
+    AnswerOffer(rings_core::error::Error) = 208,
+    #[error("Accept answer info failed: {0}.")]
+    AcceptAnswer(rings_core::error::Error) = 209,
+    #[error("Decode error.")]
+    DecodeError = 300,
+    #[error("Encode error.")]
+    EncodeError = 301,
+    #[error("WASM compile error: {0}")]
+    WasmCompileError(String) = 400,
+    #[error("BackendMessage RwLock Error")]
+    WasmBackendMessageRwLockError = 401,
+    #[error("WASM instantiation error.")]
+    WasmInstantiationError = 402,
+    #[error("WASM export error.")]
+    WasmExportError = 403,
+    #[error("WASM runtime error: {0}")]
+    WasmRuntimeError(String) = 404,
+    #[error("WASM global memory mutex error.")]
+    WasmGlobalMemoryLockError = 405,
+    #[error("WASM failed to load file.")]
+    WasmFailedToLoadFile = 406,
     #[error("Invalid did.")]
-    InvalidDid,
+    InvalidDid = 500,
     #[error("Invalid method.")]
-    InvalidMethod,
+    InvalidMethod = 501,
     #[error("Internal error.")]
-    InternalError,
-    #[error("Connect error, {0}")]
-    ConnectError(rings_core::error::Error),
-    #[error("Send message error: {0}")]
-    SendMessage(rings_core::error::Error),
+    InternalError = 502,
     #[error("No Permission")]
-    NoPermission,
+    NoPermission = 504,
+    #[error("Connect error, {0}")]
+    ConnectError(rings_core::error::Error) = 600,
+    #[error("Send message error: {0}")]
+    SendMessage(rings_core::error::Error) = 601,
     #[error("vnode action error: {0}")]
-    VNodeError(rings_core::error::Error),
+    VNodeError(rings_core::error::Error) = 603,
     #[error("service register action error: {0}")]
-    ServiceRegisterError(rings_core::error::Error),
+    ServiceRegisterError(rings_core::error::Error) = 604,
     #[error("JsError: {0}")]
-    JsError(String),
-    #[error("Invalid http request: {0}")]
-    HttpRequestError(String),
+    JsError(String) = 700,
     #[error("Invalid message")]
-    InvalidMessage,
+    InvalidMessage = 800,
+    #[error("Invalid http request: {0}")]
+    HttpRequestError(String) = 801,
     #[error("Invalid data")]
-    InvalidData,
+    InvalidData = 802,
     #[error("Invalid service")]
-    InvalidService,
+    InvalidService = 803,
     #[error("Invalid address")]
-    InvalidAddress,
+    InvalidAddress = 804,
     #[error("Invalid auth data")]
-    InvalidAuthData,
-    #[error("Storage Error: {0}")]
-    Storage(rings_core::error::Error),
-    #[error("Swarm Error: {0}")]
-    Swarm(rings_core::error::Error),
-    #[error("Create File Error: {0}")]
-    CreateFileError(String),
-    #[error("Open File Error: {0}")]
-    OpenFileError(String),
-    #[error("acquire lock failed")]
-    Lock,
+    InvalidAuthData = 805,
     #[error("invalid headers")]
-    InvalidHeaders,
+    InvalidHeaders = 806,
+    #[error("Storage Error: {0}")]
+    Storage(rings_core::error::Error) = 807,
+    #[error("Swarm Error: {0}")]
+    Swarm(rings_core::error::Error) = 808,
+    #[error("Create File Error: {0}")]
+    CreateFileError(String) = 900,
+    #[error("Open File Error: {0}")]
+    OpenFileError(String) = 901,
+    #[error("acquire lock failed")]
+    Lock = 902,
     #[error("serde json error: {0}")]
-    SerdeJsonError(#[from] serde_json::Error),
+    SerdeJsonError(#[from] serde_json::Error) = 1000,
     #[error("verify error: {0}")]
-    VerifyError(String),
+    VerifyError(String) = 1001,
 }
 
 impl Error {
-    fn discriminant(&self) -> u16 {
-        // SAFETY: Because `Self` is marked `repr(u16)`, its layout is a `repr(C)` `union`
-        // between `repr(C)` structs, each of which has the `u16` discriminant as its first
+    fn discriminant(&self) -> u32 {
+        // SAFETY: Because `Self` is marked `repr(u32)`, its layout is a `repr(C)` `union`
+        // between `repr(C)` structs, each of which has the `u32` discriminant as its first
         // field, so we can read the discriminant without offsetting the pointer.
         // This code is copy from
         // ref: https://doc.rust-lang.org/std/mem/fn.discriminant.html
-        // And we modify it from [u8] to [u16], this is work because
+        // And we modify it from [u8] to [u32], this is work because
         // repr(C) is equivalent to one of repr(u*) (see the next section) for
         // fieldless enums.
         // ref: https://doc.rust-lang.org/nomicon/other-reprs.html
-        unsafe { *<*const _>::from(self).cast::<u16>() }
+        unsafe { *<*const _>::from(self).cast::<u32>() }
     }
 
-    pub fn code(&self) -> u16 {
+    pub fn code(&self) -> u32 {
         self.discriminant()
     }
 }
@@ -145,6 +147,6 @@ mod tests {
     #[test]
     fn test_error_code() {
         let err = Error::RemoteRpcError("Test".to_string());
-        assert_eq!(err.code(), 0);
+        assert_eq!(err.code(), 100);
     }
 }
