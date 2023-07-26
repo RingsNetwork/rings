c0d7ae09a62cb4db0c6368e71ac0fb94b9fd2ff9
Ryan.K <ryankung@ieee.org>
2023-07-26 18:59:11 +0800

    use debug instead of to_string

--- core/src/transports/wasm/transport.rs
+++ core/src/transports/wasm/transport.rs
@@ -549,7 +549,7 @@ impl IceTrickleScheme for WasmTransport {
             .collect();
 
         if local_candidates_json.is_empty() {
-            return Err(Error::FailedOnGatherLocalCandidate(kind.to_string()));
+            return Err(Error::FailedOnGatherLocalCandidate(format!("{:?}", kind)));
         }
 
         let data = HandshakeInfo {
