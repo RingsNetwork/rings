7eea0c032f642b16b6dc5e885b915e9aa2459be4
magine <magine233@gmail.com>
2023-12-16 10:30:54 +0800

    Remove tonic, enable serde in protobuf generated code

--- rpc/build.rs
+++ rpc/build.rs
@@ -1,18 +1,7 @@
 use std::process::Command;
 
 fn main() {
-    tonic_build::configure()
-        .out_dir("src/protos")
-        .client_attribute(
-            "rings_node.RingsNodeService",
-            r#"#[cfg(not(target_arch = "wasm32"))]"#,
-        )
-        .server_attribute(
-            "rings_node.RingsNodeService",
-            r#"#[cfg(not(target_arch = "wasm32"))]"#,
-        )
-        .compile(&["src/protos/rings_node.proto"], &["src/protos"])
-        .unwrap();
+    prost_serde::build_with_serde(include_str!("src/protos/build_opts.json"));
 
     Command::new("cargo").args(["fmt"]).output().unwrap();
 
