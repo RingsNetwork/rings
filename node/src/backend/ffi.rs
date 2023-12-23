75afa8c3d478e97a0ce4e831b2d450c3559a9a9f
Ryan.K <ryankung@ieee.org>
2023-12-23 18:01:02 +0800

    simplify code with macro

--- node/src/backend/ffi.rs
+++ node/src/backend/ffi.rs
@@ -46,6 +46,25 @@ pub extern "C" fn new_ffi_backend_behaviour(
     }
 }
 
+macro_rules! handle_backend_message {
+    ($self:ident, $provider:ident, $handler:ident, $message:ident) => {
+        if let Some(handler) = $self.$handler {
+            let provider: &Provider = Arc::as_ref(&$provider);
+            let provider_ptr: ProviderPtr = provider.into();
+            let payload = serde_json::to_string(&$self.payload)?;
+            let message = serde_json::to_string(&$message)?;
+            let payload = CString::new(payload)?;
+            let message = CString::new(message)?;
+            handler(
+                $self as *const FFIBackendBehaviour,
+                &provider_ptr as *const ProviderPtr,
+                payload.as_ptr(),
+                message.as_ptr(),
+            );
+        }
+    };
+}
+
 #[async_trait]
 impl MessageEndpoint<BackendMessage> for FFIBackendBehaviour {
     async fn on_message(
@@ -56,52 +75,13 @@ impl MessageEndpoint<BackendMessage> for FFIBackendBehaviour {
     ) -> Result<()> {
         match msg {
             BackendMessage::PlainText(m) => {
-                if let Some(handler) = self.paintext_message_handler {
-                    let provider: &Provider = Arc::as_ref(&provider);
-                    let provider_ptr: ProviderPtr = provider.into();
-                    let payload = serde_json::to_string(&payload)?;
-                    let message = serde_json::to_string(&m)?;
-                    let payload = CString::new(payload)?;
-                    let message = CString::new(message)?;
-                    handler(
-                        self as *const FFIBackendBehaviour,
-                        &provider_ptr as *const ProviderPtr,
-                        payload.as_ptr(),
-                        message.as_ptr(),
-                    );
-                }
+                handle_backend_message!(self, provider, paintext_message_handler, m)
             }
             BackendMessage::Extension(m) => {
-                if let Some(handler) = self.extension_message_handler {
-                    let provider: &Provider = Arc::as_ref(&provider);
-                    let provider_ptr: ProviderPtr = provider.into();
-                    let payload = serde_json::to_string(&payload)?;
-                    let message = serde_json::to_string(&m)?;
-                    let payload = CString::new(payload)?;
-                    let message = CString::new(message)?;
-                    handler(
-                        self as *const FFIBackendBehaviour,
-                        &provider_ptr as *const ProviderPtr,
-                        payload.as_ptr(),
-                        message.as_ptr(),
-                    );
-                }
+                handle_backend_message!(self, provider, extension_message_handler, m)
             }
             BackendMessage::ServiceMessage(m) => {
-                if let Some(handler) = self.service_message_handler {
-                    let provider: &Provider = Arc::as_ref(&provider);
-                    let provider_ptr: ProviderPtr = provider.into();
-                    let payload = serde_json::to_string(&payload)?;
-                    let message = serde_json::to_string(&m)?;
-                    let payload = CString::new(payload)?;
-                    let message = CString::new(message)?;
-                    handler(
-                        self as *const FFIBackendBehaviour,
-                        &provider_ptr as *const ProviderPtr,
-                        payload.as_ptr(),
-                        message.as_ptr(),
-                    );
-                }
+                handle_backend_message!(self, provider, service_message_handler, m)
             }
         }
         Ok(())
