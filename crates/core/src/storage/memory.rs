dcf459b1ab1c0067e86e5e74a5b05435390ec58f
Ryan.K <ryankung@ieee.org>
2024-01-08 20:25:51 +0800

    fmt

--- crates/core/src/storage/memory.rs
+++ crates/core/src/storage/memory.rs
@@ -51,10 +51,7 @@ where
     }
 
     pub fn items(&self) -> Vec<(K, V)> {
-        self.table
-            .clone()
-            .into_iter()
-            .collect()
+        self.table.clone().into_iter().collect()
     }
 
     pub fn remove(&self, addr: &K) -> Option<(K, V)> {
