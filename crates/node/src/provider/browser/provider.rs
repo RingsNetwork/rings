a55370a987085d546f540af157537c45310a5ca0
Ryan.K <ryankung@ieee.org>
2024-03-14 14:43:50 +0800

    provider ref

--- crates/node/src/provider/browser/provider.rs
+++ crates/node/src/provider/browser/provider.rs
@@ -43,6 +43,53 @@ pub enum AddressType {
     Ed25519,
 }
 
+#[derive(Clone, Serialize, Deserialize)]
+pub(crate) struct Peer {
+    pub did: String,
+    pub state: String,
+}
+
+impl Peer {
+    fn new(did: Did, state: WebrtcConnectionState) -> Self {
+        Self {
+            did: did.to_string(),
+            state: format!("{:?}", state),
+        }
+    }
+}
+
+impl TryFrom<&Peer> for JsValue {
+    type Error = JsError;
+
+    fn try_from(value: &Peer) -> Result<Self, Self::Error> {
+        js_value::serialize(value).map_err(JsError::from)
+    }
+}
+
+
+/// A wrapper of Arc Ref of Provider
+#[derive(Clone)]
+#[wasm_export]
+pub struct ProviderRef {
+    inner: Arc<Provider>
+}
+
+impl ProviderRef {
+    pub fn inner(&self) -> Arc<Provider> {
+	self.inner.clone()
+    }
+}
+
+#[wasm_export]
+impl Provider {
+    /// make provider as an As arc ref
+    pub fn as_ref(&self) -> ProviderRef {
+	ProviderRef {
+	    inner: Arc::new(self.clone())
+	}
+    }
+}
+
 #[wasm_export]
 impl Provider {
     /// Create new instance of Provider, return Promise
