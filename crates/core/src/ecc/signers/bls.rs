452102c595f352c9ce246c45431f77a68e092f02
Ryan.K <ryankung@ieee.org>
2024-05-27 17:49:58 +0800

    bls public key

--- crates/core/src/ecc/signers/bls.rs
+++ crates/core/src/ecc/signers/bls.rs
@@ -5,9 +5,12 @@
 use bls12_381::Scalar;
 use bls_signatures  as bls;
 use libsecp256k1;
+use bls12_381::G1Affine;
+use crate::ecc::PublicKey;
 use crate::ecc::SecretKey;
 use crate::error::Error;
 use crate::error::Result;
+use bls12_381::G1Projective;
 
 impl TryInto<SecretKey> for bls::PrivateKey {
     type Error = Error;
@@ -28,5 +31,28 @@ impl TryInto<bls::PrivateKey> for SecretKey {
         } else {
             Err(Error::PrivateKeyBadFormat)
         }
+    }
+}
+
+
+impl TryInto<PublicKey> for bls::PublicKey {
+    type Error = Error;
+    fn try_into(self) -> Result<PublicKey> {
+	let data: [u8; 48] = self.as_affine().to_compressed();
+	Ok(PublicKey(data.to_vec()))
+    }
+}
+
+
+impl TryInto<bls::PublicKey> for PublicKey {
+    type Error = Error;
+    fn try_into(self) -> Result<bls::PublicKey> {
+	let data: [u8; 48] = self.0.try_into().map_err(|_| Error::PublicKeyBadFormat)?;
+	let affine: Option<G1Affine> = G1Affine::from_compressed(&data).into();
+	if let Some(af) = affine {
+	    Ok(bls::PublicKey::from(G1Projective::from(af)))
+	} else {
+	    Err(Error::PublicKeyBadFormat)
+	}
     }
 }
