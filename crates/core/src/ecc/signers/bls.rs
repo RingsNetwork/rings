f03deca857bf4c7430316140a5f60bf10bf1c45b
Ryan.K <ryankung@ieee.org>
2024-05-27 22:51:44 +0800

    implement sign

--- crates/core/src/ecc/signers/bls.rs
+++ crates/core/src/ecc/signers/bls.rs
@@ -2,15 +2,17 @@
 //! based on [bls_signatures](https://docs.rs/bls-signatures/latest/bls_signatures/)
 //! This module implement transform of [libsecp256k1::Secretkey] and [bls_signature::PrivateKey]
 
-use bls12_381::Scalar;
-use bls_signatures  as bls;
-use libsecp256k1;
-use bls12_381::G1Affine;
 use crate::ecc::PublicKey;
 use crate::ecc::SecretKey;
 use crate::error::Error;
 use crate::error::Result;
+use bls12_381::G1Affine;
 use bls12_381::G1Projective;
+use bls12_381::G2Affine;
+use bls12_381::G2Projective;
+use bls12_381::Scalar;
+use bls_signatures as bls;
+use libsecp256k1;
 
 impl TryInto<SecretKey> for bls::PrivateKey {
     type Error = Error;
@@ -34,25 +36,38 @@ impl TryInto<bls::PrivateKey> for SecretKey {
     }
 }
 
-
 impl TryInto<PublicKey> for bls::PublicKey {
     type Error = Error;
     fn try_into(self) -> Result<PublicKey> {
-	let data: [u8; 48] = self.as_affine().to_compressed();
-	Ok(PublicKey(data.to_vec()))
+        let data: [u8; 48] = self.as_affine().to_compressed();
+        Ok(PublicKey(data.to_vec()))
     }
 }
-
 
 impl TryInto<bls::PublicKey> for PublicKey {
     type Error = Error;
     fn try_into(self) -> Result<bls::PublicKey> {
-	let data: [u8; 48] = self.0.try_into().map_err(|_| Error::PublicKeyBadFormat)?;
-	let affine: Option<G1Affine> = G1Affine::from_compressed(&data).into();
-	if let Some(af) = affine {
-	    Ok(bls::PublicKey::from(G1Projective::from(af)))
-	} else {
-	    Err(Error::PublicKeyBadFormat)
-	}
+        let data: [u8; 48] = self.0.try_into().map_err(|_| Error::PublicKeyBadFormat)?;
+        let affine: Option<G1Affine> = G1Affine::from_compressed(&data).into();
+        if let Some(af) = affine {
+            Ok(bls::PublicKey::from(G1Projective::from(af)))
+        } else {
+            Err(Error::PublicKeyBadFormat)
+        }
+    }
+}
+
+/// Sign message with bls privatekey
+/// reimplemented via https://docs.rs/bls-signatures/latest/src/bls_signatures/key.rs.html#103
+/// signature = hash_into_g2(message) * sk
+pub fn sign(sec: SecretKey, hash: &[u8; 96]) -> Result<[u8; 96]> {
+    let sk: bls::PrivateKey = sec.try_into()?;
+    let affine: Option<G2Affine> = G2Affine::from_compressed(hash).into();
+    if let Some(af) = affine {
+        let mut proj: G2Projective = af.into();
+        proj *= Into::<Scalar>::into(sk);
+        Ok(G2Affine::from(proj).to_compressed())
+    } else {
+        Err(Error::PublicKeyBadFormat)
     }
 }
